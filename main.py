58e13ad9280e3117371df633459741b49633e377
SaurabhDubey <isaurabhdubey19@gmail.com>
2026-05-22 19:51:00 +0530

    upadating depricated method from langchain_community.vectorestore to new langchain_chroma for data Retrival

--- main.py
+++ main.py
@@ -3,35 +3,36 @@ from langchain_community.document_loaders import CSVLoader
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
-from langchain_community.vectorstores import Chroma
-# from langchain.chains import RetrievalQA
+from langchain_chroma import Chroma
+
 from dotenv import load_dotenv 
 import os
 load_dotenv()
 
 
+# Loading the PDF content 
 loader = PyPDFLoader("data/Saurabh_Dubey.pdf")
 page = loader.load()
 
 
 
-
+# Split the text using RecursiveCharacterTextSplitter 
 splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 250,
-    chunk_overlap = 100)
+    chunk_size = 512,
+    chunk_overlap = 0)
 
 chunks = splitter.split_documents(page)
 print(f"Total Length :",len(chunks))
 
 
 
-
+#Using Huggingface Embeddings for Embedding
 embeddings = HuggingFaceEndpointEmbeddings(
     model = "sentence-transformers/all-mpnet-base-v2",
     huggingfacehub_api_token = os.getenv("HF_TOKEN"),
 )
 
-
+#storing the chunks and embeddings in Chroma Database
 vectorstore = Chroma.from_documents(
     documents =chunks,
     embedding =  embeddings,
@@ -39,13 +40,24 @@ vectorstore = Chroma.from_documents(
 )
 
 
-
-query = "what is the document about?"
-result = vectorstore.similarity_search(query,k=3)
+# Querry to Search in Knowledgebase
+query = input("enter the Query :\n")
+result = vectorstore.similarity_search(query,k=1)
 
 for r in result:
     print(r.page_content)
     print("....")
 
 
-print("successfully stored in ChromaDB..!")
+
+## Loading the Saved ChromaDB
+vectorstore = Chroma(
+    persist_directory = "./chroma_db",
+    embedding_function = HuggingFaceEndpointEmbeddings(
+        model = "sentence-transformers/all-mpnet-base-v2",
+        huggingfacehub_api_token = os.getenv("HF_TOKEN"),)
+)
+
+
+
+
